ff806ebedc844e4b68a3fafe3bebfb575a4946e3
Orange Baron <orangebaronroblox@gmail.com>
2017-07-24 17:41:49 -0400

    m

--- server/main.go
+++ server/main.go
@@ -10,8 +10,7 @@ var playerList []players.Player
 func main() {
 	quitChan := make(chan struct{})
 	networkPlayerList=[]players.NetworkPlayer{players.NewNetworkPlayer("uwe",3,"192.168.1.25")}
-	serverFunc(quitChan)
-	go gameLoop()
+	go serverFunc(quitChan)
 	fmt.Println("aaaaaaaaa")
-	for {}
+	gameLoop()
 }
