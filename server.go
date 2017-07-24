61071fa3dcec042361dd836e2437a0296521b0a3
orangebaron <saalws@ctemc.org>
2017-07-23 23:14:49 -0400

    server things again

--- server.go
+++ server.go
@@ -1,12 +1,20 @@
 package main
 
+import "net" //now look at this net that I just found
+
+func handleConn(conn net.Conn) {
+	//do things
+}
+
 func serverFunc(quitChan chan struct{}) {
+	ln, _ := net.Listen("tcp", ":8080")
 	for {
 		select {
 		case <-quitChan:
 			return
 		default:
-			//do things
+			conn, _ := ln.Accept()
+			go handleConn(conn)
 		}
 	}
 }
