415571ba4d92f5f0f439dd58f1413c227871e760
orangebaron <saalws@ctemc.org>
2017-07-23 23:03:39 -0400

    made water an empty struct

--- water.go
+++ water.go
@@ -1,8 +1,13 @@
 package main
 
-type water struct {
-	basicBuilding
-}
+type water struct{}
 
-func (*water) getType() string { return "water" }
-func (*water) getRent() int    { return 0 }
+func (*water) getType() string              { return "water" }
+func (*water) getOwner() *player            { return nil }
+func (*water) getRevenue() int              { return 0 }
+func (*water) getRent() int                 { return 0 }
+func (*water) getClosed() bool              { return false }
+func (*water) setClosed(bool)               {}
+func (*water) routinelyCalledFunc(int, int) {}
+func (*water) getInternalInt(string) int    { return 0 }
+func (*water) setInternalInt(string, int)   {}
