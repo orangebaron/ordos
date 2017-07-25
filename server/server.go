68a9ef124b11932ce8d3efa3bf6aeb5eb3462cf9
Orange Baron <orangebaronroblox@gmail.com>
2017-07-24 23:29:12 -0400

    screw it

--- server/server.go
+++ server/server.go
@@ -43,7 +43,6 @@ func chatFunc(w http.ResponseWriter, r *http.Request) {
 		break
 	}
 
-	chat = strings.Replace(chat,"&","&amp;",1)
 	chat = strings.Replace(chat,"<","&lt;",-1)
 	chat = strings.Replace(chat,">","&gt;",-1)
 
