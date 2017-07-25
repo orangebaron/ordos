05bb13cd580f16272bed0934b58d0ea26ccaebdd
Orange Baron <orangebaronroblox@gmail.com>
2017-07-24 23:07:16 -0400

    jsss

--- server/client/ordos.js
+++ server/client/ordos.js
@@ -1,4 +1,5 @@
 const serverUrl = "http://192.168.1.14"
+const myName = "uwe" // change later
 
 function addToChat(speaker,msg) {
   document.getElementById("chatbar").innerHTML += '<p class="chat"><b class="' +
@@ -8,6 +9,15 @@ function addToChat(speaker,msg) {
 function eventRecieved(data,status) {
   console.log(data,status)
   $.get(serverUrl+":8081/event",eventRecieved) //server waits until there's new info to give then gives it
+
+  if (data.substring(0,4) == "CHAT") {
+    data = data.substring(4)
+    colon = data.search(":")
+    if (colon == -1) {
+      return
+    }
+    addToChat(data.substring(0,colon),data.substring(colon+2))
+  }
 }
 
 $(document).ready(function() {
@@ -15,7 +25,7 @@ $(document).ready(function() {
     if (key.key=="Enter") {
       var str = document.getElementById("chatbox").value
       document.getElementById("chatbox").value = ""
-      addToChat("Me",str)
+      //addToChat("Me",str)
       $.post(serverUrl+":8081/chat",str,function(){console.log("sent chat")})
     }
   }
